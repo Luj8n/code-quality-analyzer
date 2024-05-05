070d91a5ba0e993dd692623354639c73b84b2b1b
Luj8n <luj8n@duck.com>
2024-05-05 19:24:41 +0200

    Javadoc for Main

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,34 @@ import java.util.Comparator;
 import java.util.List;
 import nodes.Function;
 
+/**
+ * The main class.
+ */
 public class Main {
+    /**
+     * Main method to analyze Java files in a directory.
+     *
+     * @param args Command-line arguments. The first argument should be the directory to analyze.
+     */
     public static void main(String[] args) {
         if (args.length == 0) {
             System.err.println("Please provide a directory name to analyze");
             System.exit(1);
         }
         String directoryName = args[0];
-        List<JavaFile> files = getJavaFiles(Path.of(directoryName));
 
-        analyze(files);
+        List<JavaFile> files = getJavaFiles(Path.of(directoryName));
+        List<Function> functions = getAllFunctions(files);
+
+        analyzeFunctions(functions);
     }
 
-    private static void analyze(List<JavaFile> files) {
-        List<Function> functions = new ArrayList<>();
-
-        for (JavaFile file : files) {
-            functions.addAll(file.getFunctions());
-        }
-
+    /**
+     * Analyzes a list of functions, printing out their complexity and other metrics.
+     *
+     * @param functions List of functions to analyze.
+     */
+    private static void analyzeFunctions(List<Function> functions) {
         if (functions.isEmpty()) {
             System.out.println("No functions found");
             return;
@@ -44,6 +53,7 @@ public class Main {
                 "Score = " + function.calculateComplexity() + ", function = " + function);
         }
 
+        // Count number of functions whose names are not camelCase.
         int notCamelCase = 0;
         for (Function function : functions) {
             if (!function.isCamelCase()) {
@@ -52,10 +62,31 @@ public class Main {
         }
 
         double percentageNotCamelCase = 100.0 * notCamelCase / functions.size();
-
         System.out.printf("Not camelcase: %.2f%%", percentageNotCamelCase);
     }
 
+    /**
+     * Retrieves all functions from a list of Java files.
+     *
+     * @param files List of Java files.
+     * @return List of functions extracted from the Java files.
+     */
+    private static List<Function> getAllFunctions(List<JavaFile> files) {
+        List<Function> functions = new ArrayList<>();
+
+        for (JavaFile file : files) {
+            functions.addAll(file.getFunctions());
+        }
+
+        return functions;
+    }
+
+    /**
+     * Recursively retrieves all Java files from a directory.
+     *
+     * @param directory Path of the directory to search for Java files.
+     * @return List of Java files found in the directory.
+     */
     private static List<JavaFile> getJavaFiles(Path directory) {
         List<JavaFile> files = new ArrayList<>();
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
