e901dc7467e5a3719e3f36f464dfc82022a635a9
Luj8n <luj8n@duck.com>
2024-05-05 20:17:44 +0200

    Cleanup

--- src/nodes/Function.java
+++ src/nodes/Function.java
@@ -5,19 +5,7 @@ import java.util.List;
 import tokens.Token;
 import tokens.WordToken;
 
-public class Function {
-    private final WordToken name;
-    private final List<Token> parameters;
-    private final List<Token> inside;
-    private final Path path;
-
-    public Function(WordToken name, List<Token> parameters, List<Token> inside, Path path) {
-        this.name = name;
-        this.parameters = parameters;
-        this.inside = inside;
-        this.path = path;
-    }
-
+public record Function(WordToken name, List<Token> parameters, List<Token> inside, Path filePath) {
     public int calculateComplexity() {
         int count = 0;
         for (Token token : inside) {
@@ -36,24 +24,8 @@ public class Function {
         return word.matches("^[a-z][a-z0-9]*([A-Z][a-z0-9]+)*[A-Za-z0-9]?$");
     }
 
-    public WordToken getName() {
-        return name;
-    }
-
-    public List<Token> getParameters() {
-        return parameters;
-    }
-
-    public List<Token> getInside() {
-        return inside;
-    }
-
-    public Path getPath() {
-        return path;
-    }
-
     @Override
     public String toString() {
-        return name.getWord() + "(" + path.getFileName() + ":" + name.getLineNumber() + ")";
+        return name.getWord() + "(" + filePath.getFileName() + ":" + name.getLineNumber() + ")";
     }
 }
